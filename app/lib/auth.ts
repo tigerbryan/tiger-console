7f5027c0cd446fe12cae04b9de9dc8d1191bbe18
Bryan Chen <bryan.bingcheng@gmail.com>
2025-04-29 23:47:14 +1000

    chore: 更新用户名为 tiger

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -9,24 +9,21 @@ export interface User {
   twoFactorEnabled: boolean;
 }
 
-// 生成一个新的密钥
-const generateSecret = () => authenticator.generateSecret();
+function generateSecret() {
+  return authenticator.generateSecret();
+}
+
+export function verifyTOTP(token: string, secret: string) {
+  return authenticator.verify({ token, secret });
+}
 
 export const users: User[] = [
   {
     id: "1",
-    name: "Tiger Admin",
+    name: "tiger",
     email: "admin@tigerkits.com",
     password: "tiger@2024",
     twoFactorSecret: generateSecret(),
     twoFactorEnabled: true,
   },
-];
-
-export const verifyTOTP = (token: string, secret: string) => {
-  try {
-    return authenticator.verify({ token, secret });
-  } catch {
-    return false;
-  }
-}; 
+]; 
