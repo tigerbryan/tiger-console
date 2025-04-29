bed80215ceaf7037449c84d6a5576ed67e0e7486
Bryan Chen <bryan.bingcheng@gmail.com>
2025-04-29 23:11:36 +1000

    refactor: 分离客户端和服务器端组件

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import React from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ClientLayout from './components/ClientLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
