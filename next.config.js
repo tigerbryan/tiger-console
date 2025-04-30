ccdf006187c6738040c9bcb3fb5c15762e218a0a
tigerbryan <tigerbryan@163.com>
2025-05-01 07:50:49 +0800

    添加Next.js配置和公共资源

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '43.100.16.213',
+      }
+    ]
+  },
+}
+
+module.exports = nextConfig 
